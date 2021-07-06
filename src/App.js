702ad597af30c64672364c9a5ade426ce1a73bb8
Divya Chouhan <divyajchouhan@icloud.com>
2021-07-07 00:24:27 +0100

    search dynamic recipes

--- src/App.js
+++ src/App.js
@@ -8,21 +8,32 @@ function App() {
   const appKey = process.env.REACT_APP_API_KEY;;
 
   const [recipes, setRecipes] = useState([]);
-  
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     getRecipes();
-  });
+  }, [query]);
 
   const getRecipes = () => {
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=salmon&app_id=${appId}&app_key=${appKey}`)
+    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${appId}&app_key=${appKey}`)
       .then(response => response.json())
       .then(data => setRecipes(data.hits));
+  };
+
+  const updateSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const getQuery = (event) => {
+    event.preventDefault();
+    setQuery(search);
   }
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input type="text" className="search-bar"/>
+      <form className="search-form" onSubmit={getQuery}>
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
         <button type="submit" className="search-button">
           Search
         </button>
