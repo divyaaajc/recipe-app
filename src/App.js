2451c1c27bea062e78225602df4f4a674e06a069
Divya Chouhan <divyajchouhan@icloud.com>
2021-07-06 23:44:07 +0100

    key and id added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 
 
 function App() {
+  const appId = process.env.REACT_APP_APP_ID;;
+  const appKey = process.env.REACT_APP_API_KEY;;
 
-  const endpoint = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${appId}&app_key=${appKey}`;
-
-  const [counter, setCounter] = useState(0);
+  const recipes
 
   useEffect(() => {
-    console.log('Effect has been run');
+    getRecipes();
   });
+
+  const getRecipes = () => {
+    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=salmon&app_id=${appId}&app_key=${appKey}`)
+      .then(response => response.json())
+      .then(data => console.log(data.hits));
+  }
 
   return (
     <div className="App">
